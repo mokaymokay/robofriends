6a8ecc17c1d904f3030f4b55ac28f9fbef5f3179
Kay Mok <kaymok3@gmail.com>
2018-05-29 13:07:21 -0400

    Render multiple robot cards; delete unnecessary files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/test?200x200" alt="robot" />
+      <div>
+        <h2>mokay</h2>
+        <p>mokay@react.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
