085046dc5a9fea0762bf090ce46fe0e224243221
Kay Mok <kaymok3@gmail.com>
2018-05-29 14:35:47 -0400

    Add SearchBox and state to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
 
-const App = () => {
-  return (
-    <div>
-      <h1 className="tc">RoboFriends</h1>
-      <CardList robots={robots}/>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={this.state.robots}/>
+      </div>
+    )
+  }
 }
 
 export default App;
