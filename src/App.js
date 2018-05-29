31f98dd09fbbcb8f1ad08cb1b59dc1ab3d53cb1d
Kay Mok <kaymok3@gmail.com>
2018-05-29 16:06:58 -0400

    Render robots dynamically by fetching data from placeholder JSON file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
-      searchfield: ''
+      robots: [],
+      searchfield: ""
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
+    }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
