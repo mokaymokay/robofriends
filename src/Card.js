275f68f62d6d1ae862f24335f569dc07bb79f189
Kay Mok <kaymok3@gmail.com>
2018-05-29 13:34:59 -0400

    Further destructure within params

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Card = (props) => {
-  const { id, name, email } = props;
+const Card = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="robot" src={`https://robohash.org/${name}${id}?200x200`} />
